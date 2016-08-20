8babd994a646d4af74bca37222c2a7ecb2d0388c
Charles Burton <charles.d.burton@gmail.com>
2016-08-19 23:00:04 -0600

    added topic struct

--- structs/jsonstructs.go
+++ structs/jsonstructs.go
@@ -4,6 +4,11 @@ type Token struct {
 	Token string `json:"token"`
 }
 
+type Topic struct {
+	Token string `json:"token"`
+	Topic string `json:"topic"`
+}
+
 type Reading struct {
 	Reading string `json:"reading"`
 	Time    string `json:"time"`
